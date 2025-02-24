561875eaf2ae110873199e37ee8143477494c753
Denis Shakhov <denis.sh23@gmail.com>
2025-02-24 20:49:57 +0300

    add solutions

---
+++ solution/1.py
@@ -0,0 +1,4 @@
+date_name = input('Введите дату: ')
+task_name = input('Введите задачу: ')
+
+print(date_name, task_name)
---
+++ solution/3.py
@@ -0,0 +1,14 @@
+date_1 = input('Введите дату: ')
+task_1 = input('Введите задачу: ')
+
+date_2 = input('Введите дату: ')
+task_2 = input('Введите задачу: ')
+
+date_3 = input('Введите дату: ')
+task_3 = input('Введите задачу: ')
+
+dictionary = {
+    date_1: task_1,
+    date_2: task_2,
+    date_3: task_3
+}
