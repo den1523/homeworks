08710bf57e263af92ecc96cb4b0ac95b6d20a5b8
Den Shakhov <122207086+den1523@users.noreply.github.com>
2025-02-25 10:46:25 +0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -11,12 +11,18 @@ while run:
     command = input("Введите команду: ")
     if command == "help":
         print(HELP)
+    elif command == "add":
+        date = input("Введите дату: ")
+        task = input("Введите название задачи: ")
+        if date == 'Сегодня':
+            today.append(task)
+        elif date == "Завтра":
+            tomorrow.append(task)
+        else:
+            other.append(task)
+        print(f'Задача {task} добавлена!')
     elif command == "show":
         print(tasks)
-    elif command == "add":
-        task = input("Введите название задачи: ")
-        tasks.append(task)
-        print("Задача добавлена!")
     elif command == "exit":
         print("Спасибо за использование! До свидания!")
         break
